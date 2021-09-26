dfee763affac54fb8aa62ae30963bcdc3f2def58
Kamil <kamilabdullaev1991@gmail.com>
2021-09-26 18:37:06 +0300

    Sorting, filtring, searching and pagination add

--- API/Middleware/ExceptionMiddleware.cs
+++ API/Middleware/ExceptionMiddleware.cs
@@ -1,8 +1,5 @@
-using System.Runtime.Serialization.Json;
 using System.Net;
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Hosting;
--- API/Startup.cs
+++ API/Startup.cs
@@ -29,7 +29,13 @@ namespace API
 
             services.AddApplicationServices();
             services.AddSwaggerDocumentation();
-
+            services.AddCors(opt =>
+            {
+                opt.AddPolicy("CorsPolicy", policy =>
+                {
+                    policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200");
+                });
+            });
 
         }
 
@@ -50,6 +56,8 @@ namespace API
 
             app.UseRouting();
             app.UseStaticFiles();
+
+            app.UseCors("CorsPolicy");
 
             app.UseAuthorization();
 
