71f8aa424f0e4b5f85fefb9e9ef835f9fb9d4ac3
Kamil <kamilabdullaev1991@gmail.com>
2021-10-11 22:44:55 +0300

    View orders component add

---
+++ client/src/app/orders/orders-routing.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { OrdersComponent } from 'src/app/orders/orders.component';
+import { RouterModule, Routes } from '@angular/router';
+import { OrderDetailedComponent } from 'src/app/orders/order-detailed/order-detailed.component';
+
+const routes: Routes = [
+  { path: '', component: OrdersComponent },
+  {
+    path: ':id',
+    component: OrderDetailedComponent,
+    data: { breadcrumb: { alias: 'OrderDetailed' } },
+  },
+];
+
+@NgModule({
+  declarations: [],
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
+})
+export class OrdersRoutingModule {}
